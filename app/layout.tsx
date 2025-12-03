5149baf1d8c0c2fcf17456cfdadf1babcc2ebafe
stemaco <stema.stores@gmail.comgit commit --amend --reset-author --no-edit>
2025-12-03 12:21:38 +0530

    Change app title to WACE

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'WACE',
+  description: 'WACE - Collaborative workspace platform',
   generator: 'v0.app',
   icons: {
     icon: '/logo.png',
